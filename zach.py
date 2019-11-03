65b00e22b43748b6f836a730dfebc9aefad92c59
Bidhu Das <biddas@cisco.com>
2019-11-03 12:57:35 -0500

    made changed on zach.py

--- zach.py
+++ zach.py
@@ -4,7 +4,7 @@ def filebuilder(x):
 
 def main():
     print("Zach is the instructor for the week")
-    print("We should probably find youtube.com/user/alta3resarch and subscribe to his page!")
+    print("We should find youtube.com/user/alta3resarch and subscribe to his page!")
     filebuilder("Zach")
 
 if __name__ == "__main__":
